0ab075482577645f1830a17c5c69a7bdd9a9e73f
Suayb Kardas <kardassuayb@gmail.com>
2023-08-29 00:28:52 +0300

    propTypes package added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import Button from "./Button";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <div>
+        <Button>
+          <div>
+            <input />
+          </div>
+        </Button>
+      </div>
+      <div>
+        <Button>Send</Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+      <div>
+        <Button></Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,14 @@
+const Button = ({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) => {
+  return <button>{children}</button>;
+};
+
+export default Button;
