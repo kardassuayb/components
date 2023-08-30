ae3f9672312d9d8f7a7d4564145f9301b88e0f1a
Suayb Kardas <kardassuayb@gmail.com>
2023-08-31 00:35:02 +0300

    classnames library added

--- src/Button.js
+++ src/Button.js
@@ -1,4 +1,4 @@
-import PropTypes from "prop-types";
+import classNames from "classnames";
 
 const Button = ({
   children,
@@ -10,7 +10,11 @@ const Button = ({
   outline,
   rounded,
 }) => {
-  return <button>{children}</button>;
+  return (
+    <button className="px-3 py-1.5 border border-blue-00 bg-blue-500 text-white m-1">
+      {children}
+    </button>
+  );
 };
 
 Button.propTypes = {
