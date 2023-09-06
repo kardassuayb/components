219c821c94ebc935d71866fe877d098f8293af59
Suayb Kardas <kardassuayb@gmail.com>
2023-09-07 01:15:23 +0300

    Event Handler Added

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,12 +3,16 @@ import { useState } from "react";
 const Accordion = ({ items }) => {
   const [expandedIndex, setExpandedIndex] = useState(0);
 
+  const clickHandle = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  }; // Bu fonksiyonu aşağıda bulunan map fonksiyonu içerisinde tanımlayabilir, ve daha kısa kod yazarak kurtulabilirdik. Ancak bu durumda map fonksiyonu karışık bir hal alacaktı. Biz de bu nedenle dışarıda tanımladık. Dışarıda tanımlayınca map içerisindeki index'e erişebilmesi için de map fonksiyonunda bulunan JSX içerisindeki onClick metoduna arrow function ekleyerek index'i prop olarak bu fonksiyona atamış olduk.
+
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div onClick={() => clickHandle(index)}>{item.label}</div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
