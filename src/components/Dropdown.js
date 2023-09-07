579f09c394a7adb9049f044b5d5efe367a8322d8
Suayb Kardas <kardassuayb@gmail.com>
2023-09-07 20:08:31 +0300

    Dropdown Open State

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const Dropdown = ({ options }) => {
-  return <div>Dropdown</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (option) => {
+    setIsOpen(false);
+    console.log(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div onClick={handleClick}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 };
 
 export default Dropdown;
