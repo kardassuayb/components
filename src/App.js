23bd956c0ab66c6cdba1055094887e1ff3fccd58
Suayb Kardas <kardassuayb@gmail.com>
2023-09-10 00:21:21 +0300

    Navigation.js and createContext created

--- src/App.js
+++ src/App.js
@@ -1,31 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
-
 function App() {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    {
-      label: "Red",
-      value: "red",
-    },
-    {
-      label: "Green",
-      value: "green",
-    },
-    {
-      label: "Blue",
-      value: "blue",
-    },
-  ];
-
-  return (
-    <Dropdown value={selection} onChange={handleSelect} options={options} />
-  );
+  return <div></div>;
 }
 
 export default App;
