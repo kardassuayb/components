e2b94c905ce735278969c3e56b67b73abfc4f96a
Suayb Kardas <kardassuayb@gmail.com>
2023-09-09 23:15:48 +0300

    Extracting the DropdownPage

---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Dropdown from "../components/Dropdown";
+
+function DropdownPage() {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+
+  const options = [
+    {
+      label: "Red",
+      value: "red",
+    },
+    {
+      label: "Green",
+      value: "green",
+    },
+    {
+      label: "Blue",
+      value: "blue",
+    },
+  ];
+
+  return (
+    <Dropdown value={selection} onChange={handleSelect} options={options} />
+  );
+}
+
+export default DropdownPage;
