325bf80c317dd20e59195022042e8bd8287b780b
Suayb Kardas <kardassuayb@gmail.com>
2023-09-10 01:07:40 +0300

    popstate event used for clicking back:forward btn

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,8 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const NavigationContext = createContext();
 
 function NavigationProvider({ children }) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+    window.addEventListener("popstate", handler);
+
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
   return (
     <NavigationContext.Provider value={{}}>
       {children}
