5c207ae1ca74e1cec2874ece165d6690b7ed6844
Suayb Kardas <kardassuayb@gmail.com>
2023-09-12 15:42:51 +0300

    overflow-hidden when modal is open

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,26 @@
+import { useEffect } from "react";
 import ReactDOM from "react-dom";
-import Button from "./Button";
 
 const Modal = ({ onClose, children, actionBar }) => {
+  useEffect(() => {
+    document.body.classList.add("overflow-hidden");
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, []); //! Bu useEffect kancasını, modal ekranda göründüğünde sayfayı sabitleyip, modal kapanırken tekrar sayfa kaydırmayı aktif etmek için kullandık.
+
   return ReactDOM.createPortal(
     <div>
       <div
         onClick={onClose}
-        className="absolute inset-0 bg-gray-300 opacity-80"
+        className="fixed inset-0 bg-gray-300 opacity-80"
       ></div>
-      <div className="absolute inset-40 p-10 bg-white">
-        {children}
-        {actionBar}
+      <div className="fixed inset-60 p-10 bg-white">
+        <div className="flex flex-col justify-between h-full">
+          {children}
+          <div className="flex justify-end">{actionBar}</div>
+        </div>
       </div>
     </div>,
 
