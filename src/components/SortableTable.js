8c18ab4c9440c4069be5e15fca0b70562fb1f169
Suayb Kardas <kardassuayb@gmail.com>
2023-09-14 14:57:05 +0300

    Adding Sorting state and Sorting done!

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import Table from "./Table";
 
 function SortableTable(props) {
-  const { config } = props;
+  const { config, data } = props;
+
+  const [sortOrder, setSortOrder] = useState(null);
+  const [sortBy, setSortBy] = useState(null);
+
+  const handleClick = (label) => {
+    if (sortOrder === null) {
+      setSortOrder("asc");
+      setSortBy(label);
+    } else if (sortOrder === "asc") {
+      setSortOrder("desc");
+      setSortBy(label);
+    } else if (sortOrder === "desc") {
+      setSortOrder(null);
+      setSortBy(null);
+    }
+  };
 
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
@@ -10,11 +27,58 @@ function SortableTable(props) {
 
     return {
       ...column,
-      header: () => <th>{column.label} IS SORTABLE </th>,
+      header: () => (
+        <th onClick={() => handleClick(column.label)}>
+          {getIcons(column.label, sortBy, sortOrder)}
+          {column.label}
+        </th>
+      ),
     };
   });
 
-  return <Table {...props} config={updatedConfig} />;
+  // Eğer sortOrder && sortBy değerleri "null" değil ise "data" yı sırala
+  // "Data" prop'unun bir kopyasını oluştur.
+  //! Çünkü react'ta özellikle prop olarak aktarılan herhangi bir array'i doğrudan değiştirmek çok büyük bir olaydır ve bu istenmez
+  // Doğru sortValue fonksiyonunu bul ve sıralama için onu kullan
+
+  let sortedData = data;
+
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const valueA = sortValue(a);
+      const valueB = sortValue(b);
+
+      const reverseOrder = sortOrder === "asc" ? 1 : -1;
+
+      if (typeof valueA === "string") {
+        return valueA.localeCompare(valueB) * reverseOrder;
+      } else {
+        return (valueA - valueB) * reverseOrder;
+      }
+    });
+  }
+
+  return (
+    <div>
+      {sortBy} - {sortOrder}
+      <Table {...props} data={sortedData} config={updatedConfig} />
+    </div>
+  );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return "two icon";
+  }
+
+  if (sortOrder === null) {
+    return "two icon";
+  } else if (sortOrder === "asc") {
+    return "up icon";
+  } else if (sortOrder === "desc") {
+    return "down icon";
+  }
 }
 
 export default SortableTable;
