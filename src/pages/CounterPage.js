aacc5bd02cbd9ed778fe3e1970a5f82fad7c04c1
Suayb Kardas <kardassuayb@gmail.com>
2023-09-17 02:17:28 +0300

    Understanding Action Objects

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -2,11 +2,38 @@ import { useReducer } from "react";
 import Button from "../components/Button";
 import Panel from "../components/Panel";
 
+const INCREMENT_COUNT = "increment-count";
+const DECREMENT_COUNT = "decrement-count";
+const SET_VALUE_TO_ADD = "change-value";
+const ADD_VALUE = "add-value";
+
 const reducer = (state, action) => {
-  return {
-    ...state,
-    count: state.count + 1,
-  };
+  if (action.type === INCREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+  if (action.type === DECREMENT_COUNT) {
+    return {
+      ...state,
+      count: state.count - 1,
+    };
+  }
+  if (action.type === SET_VALUE_TO_ADD) {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+    };
+  }
+  if (action.type === ADD_VALUE) {
+    return {
+      ...state,
+      valueToAdd: action.payload,
+      count: state.count + state.valueToAdd,
+    };
+  }
+  return state;
 };
 
 function CounterPage({ initialCount }) {
@@ -18,21 +45,30 @@ function CounterPage({ initialCount }) {
   });
 
   const increment = () => {
-    dispatch();
+    dispatch({ type: INCREMENT_COUNT });
   };
   const decrement = () => {
     // setCount(count - 1);
+    dispatch({ type: DECREMENT_COUNT });
   };
   const handleChange = (e) => {
     const value = parseInt(e.target.value) || 0; // e.target.value değeri string döndürdüğü için bu formülle number'a çevirdik. Ayrıca bir değer girilmediyse "0" değerini alsın diye "||" kullandık.
     // setValueToAdd(value);
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     // setCount(count + valueToAdd);
     // setValueToAdd(0);
+    dispatch({
+      type: ADD_VALUE,
+    });
   };
 
+  console.log(state);
   return (
     <Panel className="m-3">
       <h1 className="text-lg">Count is {state.count}</h1>
